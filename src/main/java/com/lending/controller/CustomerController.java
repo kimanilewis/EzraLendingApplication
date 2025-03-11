69689630b0f0c35e1e1f8ca293928c59cb8ef1b8
lewis <kimanilewi@gmail.com>
2025-03-11 08:25:55 +0300

    url fixes

--- src/main/java/com/lending/controller/CustomerController.java
+++ src/main/java/com/lending/controller/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/customers")
 @Validated
 public class CustomerController {
 
